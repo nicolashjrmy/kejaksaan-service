4f8d2b521e5293ef1611eff0a2efddb24fa25574
Gery Pratama <gerypratama.o@gmail.com>
2024-06-25 16:34:59 +0700

    add: data count services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,4 +10,28 @@ export class AppController {
     const result = await this.neo4jService.read('MATCH (n) RETURN n LIMIT 25');
     return result.records.map((record) => record.get('n').properties);
   }
+
+  @Get('suspects')
+  async getSuspects() {
+    const result = await this.neo4jService.read(
+      'MATCH (n:Account) RETURN count(n) AS count',
+    );
+    return result.records.map(record => record.get('count').low);
+  }
+
+  @Get('numbers')
+  async getNumbers() {
+    const result = await this.neo4jService.read(
+      'MATCH (n:PhoneNumber) RETURN count(n) AS count',
+    );
+    return result.records.map(record => record.get('count').low);
+  }
+
+  @Get('calls')
+  async getCalls() {
+    const result = await this.neo4jService.read(
+      'MATCH (n:Call) RETURN count(n) AS count',
+    );
+    return result.records.map(record => record.get('count').low);
+  }
 }
