74fca7c46e1d666a6e45948fe8cf152906a1b879
nicolashjrmy <nicolashjeremy17@gmail.com>
2024-07-03 23:13:58 +0700

    add endpoint and remove unused endpoint

--- src/routes/informasi-buronan/informasi-buronan.controller.ts
+++ src/routes/informasi-buronan/informasi-buronan.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, UseGuards, Param } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Param, Body } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { formatResponse } from 'src/neo4j/neo4j.utils';
 import { Neo4jService } from 'src/neo4j/neo4j.service';
@@ -14,38 +14,6 @@ export class InformasiBuronanController {
     const result = await this.neo4jService.read('MATCH (n) RETURN n LIMIT 25');
     return result.records.map((record) => record.get('n').properties);
   }
-
-  @Get('suspects')
-  async getSuspects() {
-    const result = await this.neo4jService.read(
-      'MATCH (n:Buronan) RETURN count(n) AS count',
-    );
-    return result.records.map((record) => record.get('count').low);
-  }
-
-  @Get('numbers')
-  async getNumbers() {
-    const result = await this.neo4jService.read(
-      'MATCH (n:NO_HP) RETURN count(n) AS count',
-    );
-    return result.records.map((record) => record.get('count').low);
-  }
-
-  @Get('calls')
-  async getCalls() {
-    const result = await this.neo4jService.read(
-      'MATCH (n:Call) RETURN count(n) AS count',
-    );
-    return result.records.map((record) => record.get('count').low);
-  }
-
-  //   @Get('loc-buronan')
-  //   async getLocationBuronan() {
-  //     const result = await this.neo4jService.read(
-  //       'match p=(:Buronan)-[]-() where none(z in nodes(p) where z:Website) return p',
-  //     );
-  //     return result.records.map((record) => record.get('p').properties);
-  //   }
 
   @Get('one-buron')
   async getOneBuron() {
@@ -83,28 +51,6 @@ export class InformasiBuronanController {
     }));
   }
 
-  @Get('graph-email')
-  async getGraphEmail() {
-    const result = await this.neo4jService.read(
-      `match (we:Website)
-       where we.content contains $neodash_email1
-       return we.url,we.content as p`,
-    );
-    return result.records.map((record) => record.get('p').properties);
-  }
-
-  @Get('timeline')
-  async getTimeline(field) {
-    const result = await this.neo4jService.read(
-      `match p1=(a)-[:TIMELINE]->(b) 
-        with p1,a,b,
-        case when ${field} then b.dateTime else $neodash_custom1 end as rule1
-        where b.dateTime=rule1
-        return p1`,
-    );
-    return result.records.map((record) => record.get('p').properties);
-  }
-
   @Get('phone-call')
   async getPhonecall() {
     const result = await this.neo4jService.read(
@@ -130,10 +76,147 @@ export class InformasiBuronanController {
     return result.records.map((record) => record.get('p').properties);
   }
 
-  @Get('graph-profil-buron')
-  async getGraphProfilBuron() {
+  @Get('startdate')
+  async getStartDate() {
     const result = await this.neo4jService.read(
-      `MATCH p=(:Buronan)--() RETURN p LIMIT 10`,
+      `MATCH (n:Transaction_Mutation) RETURN distinct n.dateTime
+      order by n.dateTime asc`,
+    );
+    return result.records.map((record) => ({
+      Startdate: record.get('n.dateTime'),
+    }));
+  }
+
+  @Get('enddate')
+  async getEndDate() {
+    const result = await this.neo4jService.read(
+      `MATCH (n:Transaction_Mutation) RETURN distinct n.dateTime
+      order by n.dateTime desc`,
+    );
+    return result.records.map((record) => ({
+      Enddate: record.get('n.dateTime'),
+    }));
+  }
+
+  @Get('no-hp')
+  async getNoHp() {
+    const result = await this.neo4jService.read(
+      `with ["081181234455","087777812345"] as ph
+      unwind ph as phone
+      return phone`,
+    );
+    return result.records.map((record) => ({
+      NoHp: record.get('phone'),
+    }));
+  }
+
+  @Get('nik')
+  async getNik() {
+    const result = await this.neo4jService.read(
+      `with ["3174010102700009"] as ph
+      unwind ph as nik1
+      return nik1`,
+    );
+    return result.records.map((record) => ({
+      Nik: record.get('nik1'),
+    }));
+  }
+
+  @Get('no-rek')
+  async getNoRek() {
+    const result = await this.neo4jService.read(
+      `with ["2907991604"] as ph
+      unwind ph as rekening1
+      return rekening1`,
+    );
+    return result.records.map((record) => ({
+      NoRekening: record.get('rekening1'),
+    }));
+  }
+
+  @Get('email')
+  async getEmail() {
+    const result = await this.neo4jService.read(
+      `with ["harunmasiku@example.com","hmasiku@corporate.com"] as ph
+      unwind ph as email1
+      return email1`,
+    );
+    return result.records.map((record) => ({
+      Email: record.get('email1'),
+    }));
+  }
+
+  @Get('daftar-kontak')
+  async getDaftarKontak(
+    @Param('nama_buron') nama_buron: string,
+    @Param('no_hp') no_hp: string,
+    @Param('start_date') start_date: any,
+    @Param('end_date') end_date: any,
+  ) {
+    const result = await this.neo4jService.read(
+      `match (a:Buronan{nama:"${nama_buron}"})-[:PUNYA_HP]->(b:NO_HP)-[:HAS_CONTACT_PHONE]->(c:Contact_Phone)
+      where ${start_date} <= c.dateTime <= ${end_date}
+      and
+      b.no_hp[0]="${no_hp}" or b.no_hp[1]="${no_hp}"
+      return  c.added_date as Tanggal_Ditambah, c.number as No_Kontak, c.provider as Provider
+      order by Tanggal_Ditambah desc`,
+    );
+    return result.records.map((record) => ({
+      Tanggal_Ditambah: record.get('Tanggal_Ditambah'),
+      No_Kontak: record.get('No_Kontak'),
+      Provider: record.get('Provider'),
+    }));
+  }
+
+  @Get('data-ccty')
+  async getDataCctv(
+    @Param('nama_buron') nama_buron: string,
+    @Param('start_date') start_date: any,
+    @Param('end_date') end_date: any,
+  ) {
+    const result = await this.neo4jService.read(
+      `MATCH (n:CCTVData {buronan:"${nama_buron}"} where ${start_date} <= n.dateTime <= ${end_date}) 
+      where n.city ='Medan' or n.city='Balikpapan' or n.city='Makassar'
+      RETURN
+      n.recorded_date as Tanggal,
+      n.latitude as Lat,
+      n.longitude as Long,
+      n.city as Kota,
+      n.location as Location
+      order by Tanggal desc`,
+    );
+    return result.records.map((record) => ({
+      Tanggal: record.get('Tanggal'),
+      Latitude: record.get('Lat'),
+      Longitude: record.get('Long'),
+      Kota: record.get('Kota'),
+      Lokasi: record.get('Location'),
+    }));
+  }
+
+  @Get('graph-profil-buron')
+  async getGraphProfilBuron(
+    @Param('nik') nik: string,
+    @Param('no_hp') no_hp: string,
+    @Param('no_rek') no_rek: string,
+    @Param('start_date') start_date: any,
+    @Param('end_date') end_date: any,
+    @Param('email') email: string,
+    @Param('n_kontak1') n_kontak1: string,
+    @Param('tgl_cctv') tgl_cctv: string,
+  ) {
+    const result = await this.neo4jService.read(
+      `optional match p1=(bu1:Buronan)-[]->(:NIK{nik:"${nik}"})
+       optional match p2=(bu1)-[]->(n1:NO_HP where n1.no_hp[0]="${no_hp}")
+       optional match p3=(bu1)-[]->(n2:NO_HP where n2.no_hp[1]="${no_hp}")
+       optional match p4=(bu1)-[]->(:NO_REKENING{no_rekening:"${no_rek}"})
+       optional match p5=(bu1)-[]->(:EMAIL{email:"${email}"})
+       optional match p6=(a1)-[:PUNYA_HP]->(b1:NO_HP)-[:HAS_CONTACT_PHONE]->(c1:Contact_Phone where ${start_date} <= toString(c1.dateTime) <= ${end_date} and b1.no_hp[0]="${no_hp}" or b1.no_hp[1]="${no_hp}") 
+       optional match p7=(a1{no_rekening:"${no_rek}"})-[r2:HAS_TX_MUTATION]->(b2:Transaction_Mutation where ${start_date} <= b2.dateTime <= ${end_date}) 
+       optional match p8=(c1)--(n:Kaki_Tangan{phone_number:"${n_kontak1}"})-[]-(:Call_Suspicious)
+       optional match p9=(bu1)-[:TERTANGKAP_CCTV]->(:CCTVData{recorded_date:"${tgl_cctv}"})
+       WITH collect(p1) + collect(p2) + collect(p3) + collect(p4) + collect(p5) + collect(p6) + collect(p7) + collect(p8) + collect(p9) AS p
+       return p LIMIT 100`,
     );
     const formatResult = formatResponse(result.records);
     return formatResult;
