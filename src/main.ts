0ee88bc02bea6ca25f22d5aa212d10081f294d75
nicolashjrmy <nicolashjeremy17@gmail.com>
2024-07-12 09:33:42 +0700

    add certificate for secure connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./src/dintegrasi-cert/privkey.pem'),
+    cert: fs.readFileSync('./src/dintegrasi-cert/fullchain.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: '*',
